4ceb464b9827c3b3f337ca2d2e6268711e8f23e4
Lennartluber <lenlau14@kingshigh.school.nz>
2025-06-05 10:55:35 +1200

    I am working on spikes for my level layout

--- scenes/car_controller.gd
+++ scenes/car_controller.gd
@@ -7,32 +7,34 @@ var acceleration = 600.0     # Acceleration rate
 var brake_force = 800.0      # Braking force
 var turn_speed = 3.0         # Rotation speed in radians per second
 var friction = 200.0         # Slowdown when no input
-
+var controlling = true
 # Current control values
 var move_input = 0.0
 var turn_input = 0.0
 
 func _physics_process(delta):
+	if controlling : 
+		
 	# Input handling
 	# Forward (W) and reverse (S)
-	move_input = 0.0
-	if Input.is_action_pressed("up"):  # W key
-		move_input = 1.0
-	if Input.is_action_pressed("down"):  # S key
-		move_input = -1.0
-	
-	# Steering (A and D)
-	turn_input = 0.0
-	if Input.is_action_pressed("left"):  # A key
-		turn_input = -1.0
-	if Input.is_action_pressed("right"):  # D key
-		turn_input = 1.0
-	
+		move_input = 0.0
+		if Input.is_action_pressed("up"):  # W key
+			move_input = 1.0
+		if Input.is_action_pressed("down"):  # S key
+			move_input = -1.0
+		
+		# Steering (A and D)
+		turn_input = 0.0
+		if Input.is_action_pressed("left"):  # A key
+			turn_input = -1.0
+		if Input.is_action_pressed("right"):  # D key
+			turn_input = 1.0
+		rotation += turn_input * turn_speed * delta
 	# Braking (Space)
 	var is_braking = Input.is_action_pressed("ui_select")  # Space key
 	
 	# Rotate the car
-	rotation += turn_input * turn_speed * delta
+	
 	
 	# Calculate velocity
 	var direction = Vector2(cos(rotation), sin(rotation))
@@ -51,3 +53,19 @@ func _physics_process(delta):
 	
 	# Move the car
 	move_and_slide()
+
+
+
+
+func _on_puddle_slip_body_entered(body: Node2D) -> void:
+	friction = 0
+	controlling = false
+
+
+func _on_puddle_slip_body_exited(body: Node2D) -> void:
+	friction = 200
+	controlling = true
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
